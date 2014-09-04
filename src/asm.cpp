fdd709b6d3abb1459363b5a92e14bf532ed8c593
Kalinovcic <lovro.kalinovcic@gmail.com>
2014-09-05 00:53:26 +0200

    Started parsing AML code

--- src/asm.cpp
+++ src/asm.cpp
@@ -22,10 +22,117 @@
  */
 
 #include <cstdlib>
+#include <cstdio>
+#include <stdint.h>
+
+#include <iostream>
+#include <fstream>
+
+#include <map>
+
+typedef uint32_t u32;
+
+std::ifstream in;
+std::ofstream out;
+
+std::string token;
+
+u32 functionIDCounter = 0;
+u32 globalvarIDCounter = 0;
+u32 localvarIDCounter = 0;
+std::map<std::string, u32> functionIDs;
+std::map<std::string, u32> globalvarIDs;
+std::map<std::string, u32> localvarIDs;
+
+inline void error(std::string msg)
+{
+    std::cerr << msg << "\n";
+    exit(1);
+}
+
+inline u32 nextFunctionID()
+{
+    functionIDCounter++;
+    if(functionIDCounter == 0) error("function ID overflow");
+    return functionIDCounter - 1;
+}
+
+inline u32 nextGlobalvarID()
+{
+    globalvarIDCounter++;
+    if(globalvarIDCounter == 0) error("globalvar ID overflow");
+    return globalvarIDCounter - 1;
+}
+
+inline u32 nextLocalvarID()
+{
+    localvarIDCounter++;
+    if(localvarIDCounter == 0) error("var ID overflow");
+    return localvarIDCounter - 1;
+}
+
+inline bool nextToken()
+{
+    in >> token;
+    return !in.eof();
+}
+
+inline std::string nextTokenEOF()
+{
+    if(!nextToken()) error("unexpected EOF");
+    return token;
+}
+
+inline void nativeFunction(std::string name, bool isVoid)
+{
+    std::cout << "loaded native " << name << "\n";
+}
+
+inline void aspelFunction(std::string name)
+{
+    std::cout << "loaded aspfun " << name << "\n";
+}
+
+inline void function()
+{
+    if(token.size() > 4) error("invalid function def \"" + token + "\"");
+
+    bool isNative = false, isVoid = false;
+    for(unsigned int i = 2; i < token.size(); i++)
+        switch(token[i])
+        {
+        case 'n': if(isNative) error("invalid function def \"" + token + "\""); isNative = true; break;
+        case 'v': if(isVoid) error("invalid function def \"" + token + "\""); isVoid = true; break;
+        }
+
+    std::string name = nextTokenEOF();
+
+    if(isNative) nativeFunction(name, isVoid);
+    else aspelFunction(name);
+}
+
+inline void globalvar()
+{
+    std::string name = nextTokenEOF();
+
+    std::cout << "loaded globalvar " << name << "\n";
+}
 
 int main()
 {
+    in.open("../aspelc/test.aml", std::ios::in);
+    out.open("../aspelc/test.aby", std::ios::out | std::ios::binary);
 
+    while(nextToken())
+    {
+        if(token.size() < 2 || token[1] != ':')
+            error("invalid block def \"" + token + "\"");
+        switch(token[0])
+        {
+        case 'f': function(); break;
+        case 'w': globalvar(); break;
+        }
+    }
 
     return EXIT_SUCCESS;
 }
